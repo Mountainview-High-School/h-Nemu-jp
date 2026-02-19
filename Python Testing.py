16268f569da795bc6b38d3a616b839f9d5a3f814
Isabelle la grange <23lagrangei@mountainview.school.nz>
2026-02-20 10:33:58 +1300

    i was bored
    interesting transformers facts

---
+++ Python Testing.py
@@ -0,0 +1,16 @@
+#listing
+list=["a" , "b" , "c"]
+name_letter=input("name a letter")
+
+#did you know optimus prime is about roughly nine million years old?
+#did yuo know starscream was nearly kicked from his position because megatron fell inlove with a femme descepticon?
+#did you know when optimus recieves his matrix in the comics its painful for him?
+#did you know michael bay a director of many transformer movie doesnt like making female transformer characters?
+#did you know michael bay might secretly be shockwave because shockwave is hates femme bots?
+#did you know theres a transformers comic that crosses over with my little pony?
+#did you know in one of the newest comics megatron eats a tranformer?
+#did sam from transformers bayverse is dead?
+#did you know shockwave created a machine that could destroy planet system for megatron
+#did you know shockwave was a senator but was charged with crimminal charges, and was forced into a surgery that replaces his face and takes out his emotions
+#did you know soundwave only talked once during the series transformers prime
+#did you know starscream was a seeker
