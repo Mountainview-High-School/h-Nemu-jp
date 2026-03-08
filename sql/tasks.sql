d2b9047aee5a2cf30077cf20f68bb42f5dc46223
Isabelle la grange <23lagrangei@mountainview.school.nz>
2026-03-09 11:48:44 +1300

    more tasks
    trying

--- sql/tasks.sql
+++ sql/tasks.sql
@@ -1,6 +1,13 @@
 -- 1 
 select * from student order by date_of_birth;
 select * from student where first_name = 'gene';
+select * from student where last_name = 'y';
 
 insert into student (first_name, last_name, date_of_birth)
 values ("megatron" , "d-16"  , "2000-23-8");
+select * from student;
+
+insert into student (first_name, last_name, date_of_birth)
+values ("gene" , "horsey"  , "2000-16-3");
+select * from student;
+
