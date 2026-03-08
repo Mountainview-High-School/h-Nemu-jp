4706bd3403fcac4455bf03f04b247f0b47e1a4c7
Isabelle la grange <23lagrangei@mountainview.school.nz>
2026-03-09 12:02:08 +1300

    ppp

--- sql/tasks.sql
+++ sql/tasks.sql
@@ -1,7 +1,7 @@
 -- 1 
 select * from student order by date_of_birth;
 select * from student where first_name = 'gene';
-select * from student where last_name = 'y';
+select * from student where last_name = 'y%';
 
 insert into student (first_name, last_name, date_of_birth)
 values ("megatron" , "d-16"  , "2000-23-8");
