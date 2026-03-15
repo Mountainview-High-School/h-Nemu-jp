04f026d072df87e98d026fe15be834d5cd386cf7
Isabelle la grange <23lagrangei@mountainview.school.nz>
2026-03-16 11:25:24 +1300

    idk

--- sql/tasks.sql
+++ sql/tasks.sql
@@ -1,13 +1,13 @@
 -- 1 
 select * from student order by date_of_birth;
 select * from student where first_name = 'gene';
-select * from student where last_name = 'y%';
+select * from student where last_name = '%y';
+select * from class_id where not room_id = '44';
+select * from class_id where room_id ='44' or room_id = '43';
+select * from student
+
+
 
 insert into student (first_name, last_name, date_of_birth)
 values ("megatron" , "d-16"  , "2000-23-8");
 select * from student;
-
-insert into student (first_name, last_name, date_of_birth)
-values ("gene" , "horsey"  , "2000-16-3");
-select * from student;
-
