167df482e265d07f2f9797818ab3e91fa0a3099c
Isabelle la grange <23lagrangei@mountainview.school.nz>
2026-03-16 11:49:59 +1300

    help from Mr Weatherall

--- flask/webserver.py
+++ flask/webserver.py
@@ -1,7 +1,12 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+
+
+@app.route("/about.html")
+def about_page():
+    return render_template("about.html")
